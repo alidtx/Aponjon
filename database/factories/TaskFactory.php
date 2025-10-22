9764fa1d27762c48d5126df9f58ed013570c0c60
alidtx <alirimon55@gmail.com>
2025-10-22 13:40:49 +0600

    created seeder & factory for: task models

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\District;
+use App\Models\Task;
+use App\Models\User;
+use App\Models\Service;
+use App\Models\Zila;
+use App\Models\Upozila;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    public function definition(): array
+    {
+        return [
+            'task_number' => strtoupper(Str::random(10)), 
+            'customer_id' => User::inRandomOrder()->first()?->id ?? User::factory(),
+            'service_id' => Service::inRandomOrder()->first()?->id ?? Service::factory(),
+            'tasker_id' => User::inRandomOrder()->first()?->id, 
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'location_address' => $this->faker->address(),
+            'location_coordinates' => $this->faker->latitude() . ',' . $this->faker->longitude(), 
+            'district_id' => District::inRandomOrder()->first()?->id ?? District::factory(),
+            'zila_id' => Zila::inRandomOrder()->first()?->id ?? District::factory(),
+            'upozila_id' => Upozila::inRandomOrder()->first()?->id ?? Zila::factory(),
+            'scheduled_for' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'budget' => $this->faker->randomFloat(2, 100, 10000),
+            'status' => $this->faker->randomElement(['posted', 'assigned', 'in_progress', 'completed', 'cancelled', 'disputed']),
+            'customer_notes' => $this->faker->optional()->sentence(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             TaskerProfileTableSeeder::class,
             CategoryTableSeeder::class,
             ServiceTableSeeder::class,
+            TaskTableSeeder::class
         ]);
     }
 }
