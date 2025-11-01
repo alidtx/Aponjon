75d4f0d90ff6ec70ea8f5c52c32976d052f746dc
alidtx <alirimon55@gmail.com>
2025-11-01 14:10:15 +0600

    fix logo

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -3,6 +3,6 @@
         <div class="w-10 h-10 bg-primary rounded-full flex items-center justify-center">
             <i class="fas fa-hands-helping text-white text-lg"></i>
         </div>
-        <span class="text-xl font-bold text-dark">Apon<span class="text-primary">Jon</span></span>
+        <span class="text-xl font-bold text-dark">আপন<span class="text-primary">জন</span></span>
     </div>
 </template>
