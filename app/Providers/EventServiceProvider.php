34fcdbd462bc8f5f7081ed45e09835d9f1c0ffc0
alidtx <alirimon55@gmail.com>
2025-11-04 13:19:03 +0600

    removed unnecesary class

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,11 +2,7 @@
 // app/Providers/EventServiceProvider.php
 namespace App\Providers;
 
-use App\Events\OrderPlaced;
 use App\Events\UserRegistered;
-use App\Listeners\SendOrderConfirmation;
-use App\Listeners\UpdateInventory;
-use App\Listeners\NotifyWarehouse;
 use App\Listeners\SendOtpNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
