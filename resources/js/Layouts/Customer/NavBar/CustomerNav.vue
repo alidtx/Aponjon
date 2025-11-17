3ad6bf77d146cdaed9f7f807c3a9975ecc3c2a31
alidtx <alirimon55@gmail.com>
2025-11-17 18:21:54 +0600

    added gig create route

--- resources/js/Layouts/Customer/NavBar/CustomerNav.vue
+++ resources/js/Layouts/Customer/NavBar/CustomerNav.vue
@@ -3,12 +3,12 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 
 
- const props= defineProps({
-      user:{
-         type: Object,
-         required:true   
-      }
- })
+const props = defineProps({
+    user: {
+        type: Object,
+        required: true
+    }
+})
 </script>
 
 
@@ -24,12 +24,14 @@ import { Link } from '@inertiajs/vue3';
                 </div>
                 <div class="flex items-center space-x-4">
                     <a href="marketplace.html" class="text-dark hover:text-primary">টাস্কস</a>
-                    <a href="post-task.html" class="bg-accent text-white px-4 py-2 rounded-lg hover:bg-orange-600">
+                    <Link :href="route('customer.create.gig')">
+                    <span class="bg-accent text-white px-4 py-2 rounded-lg hover:bg-orange-600">
                         টাস্ক পোস্ট করুন
-                    </a>
+                    </span>
+                    </Link>
                     <div class="relative group">
                         <button class="flex items-center space-x-2">
-                            <span class="text-dark">{{props.user?.data?.name}}</span>
+                            <span class="text-dark">{{ props.user?.data?.name }}</span>
                             <img :src="props.user?.data?.avatar" alt="User Avatar"
                                 class="w-8 h-8 rounded-full object-cover border border-gray-300" />
                         </button>
