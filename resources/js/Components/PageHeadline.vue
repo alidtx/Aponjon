cab1dfd73e81060ac59b0535ec2da9c796780b2d
alidtx <alirimon55@gmail.com>
2026-01-01 15:44:27 +0600

    created comonent for page head line

--- resources/js/Components/PageHeadline.vue
+++ resources/js/Components/PageHeadline.vue
@@ -1,12 +1,32 @@
 <script setup>
-
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  highlight: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  description: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
 <template>
-    <div class="text-center mb-8">
-            <h1 class="text-4xl font-bold text-dark mb-4">সেবার <span class="text-primary max-sm">অনুরোধসমূহ</span></h1>
-            <p class="text-xl text-gray-600 max-w-3xl mx-auto">
-                বাংলাদেশের বিভিন্ন প্রান্ত থেকে আসা হাজারো সেবার অনুরোধ। আপনার দক্ষতা অনুযায়ী কাজ খুঁজে নিন।
-            </p>
-        </div>
+  <div class="text-center mb-8">
+    <h1 class="text-4xl font-bold text-dark mb-4">
+      {{ title }}
+      <span v-if="highlight" class="text-primary max-sm">
+        {{ highlight }}
+      </span>
+    </h1>
+
+    <p class="text-xl text-gray-600 max-w-3xl mx-auto">
+      {{ description }}
+    </p>
+  </div>
 </template>
