280587c55fe4716fcbb99c66f09ce41d80e660fc
alidtx <alirimon55@gmail.com>
2026-03-07 02:36:44 +0600

    removed unecesary code in task services

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -156,17 +156,5 @@ private static function applySorting($query, string $sort=null): void
 
         return $categories;
     }
-    public static function districtWiseZila()
-    {
-        $districts = District::with(['zilas:id,name,district_id'])
-            ->get(['id', 'name']);
-        return $districts;
-    }
-
-    public static function zilaWiseUpozila()
-    {
-        $zilas = Zila::with(['upozilas:id,name,zila_id'])
-            ->get(['id', 'name', 'district_id']);
-        return $zilas;
-    }
+   
 }
