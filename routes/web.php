2d81f39607bc39fbe65db7596c273f2a970ccfb2
alidtx <alirimon55@gmail.com>
2026-04-11 20:12:42 +0600

    dd

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Route::get('hh', function () {
 });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('marketplace', [HomeController::class, 'marketplace'])->name('marketplace');
-Route::get('category', [HomeController::class, 'category'])->name('category');
-Route::get('district-Wise-zila', [HomeController::class, 'districtWiseZila'])->name('district-Wise-zila');
-Route::get('Zila-Wise-upozila', [HomeController::class, 'ZilaWiseUpozila'])->name('Zila-Wise-upozila');
+Route::get('/marketplace', [HomeController::class, 'marketplace'])->name('marketplace');
+Route::get('/category', [HomeController::class, 'category'])->name('category');
+Route::get('/district-Wise-zila', [HomeController::class, 'districtWiseZila'])->name('district-Wise-zila');
+Route::get('/Zila-Wise-upozila', [HomeController::class, 'ZilaWiseUpozila'])->name('Zila-Wise-upozila');
 
 Route::get('/tasks/{taskId}/{slug}', [BidController::class, 'show'])->name('tasks.show');
 Route::get('/show-bid-submit-form/{taskId}/{slug}', [BidController::class, 'showBidSubmissionForm'])->name('show.bid.submit.form');
@@ -42,18 +42,18 @@ Route::middleware(['auth','profile_completed'])->group(function () {
     Route::middleware(['role:customer'])->group(function () {
 
         Route::prefix('customer')->name('customer.')->group(function () {
-            Route::get('dashboard', [CustomerController::class, 'index'])->name('dashboard');
-            Route::get('create-gig', [CustomerController::class, 'createGig'])->name('create.gig');
-            Route::post('gigs-store', [CustomerController::class, 'gigsStore'])->name('gigs.store');
+            Route::get('/dashboard', [CustomerController::class, 'index'])->name('dashboard');
+            Route::get('/create-gig', [CustomerController::class, 'createGig'])->name('create.gig');
+            Route::post('/gigs-store', [CustomerController::class, 'gigsStore'])->name('gigs.store');
         });
     });
 
     Route::middleware(['role:tasker'])->group(function () {
         
     Route::prefix('tasker')->name('tasker.')->group(function () {
-        Route::get('dashboard', [TaskController::class, 'index'])->name('dashboard');
-        Route::get('create-profile', [TaskController::class, 'createProfile'])->name('create.profile');
-        Route::post('store-profile', [TaskController::class, 'storeProfile'])->name('store.profile');
+        Route::get('/dashboard', [TaskController::class, 'index'])->name('dashboard');
+        Route::get('/create-profile', [TaskController::class, 'createProfile'])->name('create.profile');
+        Route::post('//store-profile', [TaskController::class, 'storeProfile'])->name('store.profile');
     });
 });
 Route::middleware('awaiting_kyc_approval')->get('/awaiting-kyc-approval', [KycApprovalController::class, 'index'])->name('kyc.awaiting-approval.index');
