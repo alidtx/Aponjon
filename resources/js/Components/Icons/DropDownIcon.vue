67a81642b491542df51f3448c079b46ca297bf81
alidtx <alirimon55@gmail.com>
2026-04-17 21:01:24 +0600

    design dropdown menu

---
+++ resources/js/Components/Icons/DropDownIcon.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="7" fill="none"
+        class="hidden w-4 shrink-0 text-gray-500 transition-transform duration-200 md:flex"
+        :class="{ 'rotate-180': open }">
+        <path fill="currentColor"
+            d="M4.24 6.114.416 1.651A1 1 0 0 1 1.175 0h7.65a1 1 0 0 1 .76 1.65L5.759 6.115a1 1 0 0 1-1.518 0Z">
+        </path>
+    </svg>
+</template>
