3447b94fa0bdd2959ba5f043669d46479038e30e
alidtx <alirimon55@gmail.com>
2026-04-27 14:49:35 +0600

    dd

--- resources/js/Layouts/MarketplaceDefaultLayout.vue
+++ resources/js/Layouts/MarketplaceDefaultLayout.vue
@@ -1,12 +1,23 @@
 <script setup>
-import Navbar from '@/Layouts/Marketplace/NavBar/NavBar.vue';
-import Footer from './Marketplace/Footer/Footer.vue';
+import NavBar from './NavBar.vue';
+import Footer from './Footer.vue';
 </script>
 
 <template>
-    <Navbar />
+    <NavBar>
+
+        <div class="flex items-center space-x-4">
+            <a href="post-task.html" class="bg-accent text-white px-4 py-2 rounded-lg hover:bg-orange-600">
+                <i class="fas fa-plus mr-2"></i>টাস্ক পোস্ট করুন
+            </a>
+            <a href="tasker-dashboard.html" class="text-dark hover:text-primary">
+                টাস্কার হোন
+            </a>
+        </div>
+
+    </NavBar>
     <div class="max-w-7xl mx-auto px-4 py-8">
-       <slot />
+        <slot />
     </div>
-    <Footer/>
+    <Footer />
 </template>
