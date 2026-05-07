9e399e82ec46fa4c7b7354107ccf567a3fb8d8df
alidtx <alirimon55@gmail.com>
2026-05-07 17:03:30 +0600

    dispute module

--- app/Http/Controllers/Bid/BidDisputedController.php
+++ app/Http/Controllers/Bid/BidDisputedController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers\Bid;
 
+use App\Enum\BidStatus;
+use App\Enum\TaskStatus;
 use App\Http\Controllers\Controller;
-
-
+use App\Http\Resources\BidResource;
+use App\Models\Bid;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class BidDisputedController extends Controller
 {
-   public function index()  {
-       dd('dd');
-   }
+    public function index()
+    {
+        $disputed = Bid::query()
+            ->where('status', BidStatus::Accepted->value)
 
+            ->whereHas('task', function ($q) {
+                $q->where('customer_id', auth()->id());
+                $q->where('status', TaskStatus::Disputed->value);
+            })
+
+            ->with([
+                'task:id,title,budget,status',
+                'tasker:id,name',
+                'tasker.taskerProfiles:id,user_id,district_id,zila_id,upozila_id'
+            ])
+            ->latest()
+            ->paginate(10);
+        return Inertia::render('Customer/Disputed/Index', [
+            'disputed' => BidResource::collection($disputed)
+        ]);
+    }
 }
---
+++ resources/js/Pages/Customer/Disputed/Index.vue
@@ -0,0 +1,36 @@
+<script setup>
+import CustomerAuthenticatedLayout from '@/Layouts/CustomerAuthenticatedLayout.vue';
+import DataTable from '@/Components/DataTable/Index.vue'
+import { ref } from 'vue';
+import { Head } from '@inertiajs/vue3';
+
+
+const tableHeader = ref([
+    { name: 'কাজের নাম', data: 'title', orderable: true, contentType: 'text' },
+    { name: 'কাজের নম্বর', data: 'task_number', orderable: true, contentType: 'text' },
+    { name: 'বাজেট', data: 'budget', orderable: true, contentType: 'text' },
+    { name: 'স্ট্যাটাস', data: 'status', orderable: true, contentType: 'text', isLabel: true },
+    { name: 'অ্যাকশন', data: 'action', orderable: false, contentType: 'slots', slotsName: 'actions' }
+])
+
+
+</script>
+
+
+
+<template>
+    <CustomerAuthenticatedLayout>
+        <Head title="বিতর্কিত" />
+        <div class="lg:col-span-3">
+            <div class="bg-white rounded-lg shadow-md p-6 mb-6">
+                <div class="flex justify-between items-center py-3">
+                    <h2 class="text-2xl font-bold text-dark">যে কাজগুলো বিতর্কিত হয়েছে </h2></div>
+                    <DataTable :tableHeader="tableHeader"
+                    @rowClicked="handleRowClick" @getFilteredResults="loadMoreData">
+                        
+                    </DataTable>        
+          </div>
+
+        </div>  
+     </CustomerAuthenticatedLayout>   
+</template>
